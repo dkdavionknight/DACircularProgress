55740393066ade8166ec689836a44daaa0ed67f1
Davion Knight <dkdavionknight@gmail.com>
2020-03-26 19:42:28 +0800

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -12,6 +12,8 @@ let package = Package(
     targets: [
         .target(
             name: "DACircularProgress",
-            dependencies: [])
+            dependencies: [],
+            sources: ["DACircularProgress"],
+            publicHeadersPath: "DACircularProgress")
     ]
 )
